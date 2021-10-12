2325e851b4e51d02649e9d1c4e7da482b10e0fcd
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-10-12 19:26:44 -0400

    added express and middleware

---
+++ server.js
@@ -0,0 +1,26 @@
+const express=require('express');
+const app=express()
+require ('dotenv').config()
+const morgan=require('morgan')
+
+app.get('/', (req, res)=>{
+
+    res.send('This is backend')
+})
+
+//middlewares
+if (process.env.NODE_ENV==='development') {
+    app.use(morgan('dev'))
+    console.log('the app is in development phase')
+
+} else if (process.env.NODE_ENV==='production') {
+
+    console.log('the app is in production phase ')
+}
+
+
+const port=process.env.PORT
+app.listen(port, ()=>{
+    console.log(`Server Running On Port: ${port}`)
+})
+
