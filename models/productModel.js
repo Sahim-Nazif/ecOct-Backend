fe4fa61803970a844869ad426bfe0034ef2b5dd8
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-10-12 21:42:43 -0400

    Reviews schema added to product model

--- models/productModel.js
+++ models/productModel.js
@@ -1,5 +1,24 @@
 const mongoose=require('mongoose')
 
+const reviewSchema=mongoose.Schema({
+
+    userid:{
+        type:mongoose.Schema.Types.ObjectID
+    },
+    name:{
+        type:String,
+        required:true
+    },
+    comment:{
+        type:String,
+        required:true
+    },
+    rating:{
+        type:Number,
+        required:true
+    }
+}, { timestamps: true })
+
 
 const productSchema=mongoose.Schema({
 
@@ -32,5 +51,10 @@ const productSchema=mongoose.Schema({
         type:Number,
         required:true
     },
-    reviews:[]
+    reviews:[reviewSchema]
 }, { timestamps: true })
+
+
+const Product=mongoose.model('products', productSchema)
+
+module.exports=Product
