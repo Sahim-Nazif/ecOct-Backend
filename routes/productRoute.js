2f75758c0d776ea92cab8123f8466a3729e3a314
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-10-16 16:17:04 -0400

    get all product routes created

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -2,10 +2,10 @@ const express=require('express');
 const router=express.Router();
 
 const {create_product}=require('../controllers/productController')
-
+const {get_all_products}=require('../controllers/productController')
 
 
 router.post('/product/create', create_product)
-
+router.get('/product/all', get_all_products)
 
 module.exports=router
