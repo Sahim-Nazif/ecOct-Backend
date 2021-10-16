cbee054ef731b261be6e6669d3db6bcea65c77ce
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-10-16 16:15:44 -0400

    created func to get all products

--- controllers/productController.js
+++ controllers/productController.js
@@ -15,7 +15,20 @@ const create_product=(req, res)=>{
 
 }
 
+const get_all_products=async(req, res)=>{
+
+    await Product.find().exec((error, products)=>{
+
+        if (error || !products){
+            return res.status(400).json({error:'Sorry we are out of stock'})
+        }
+        return res.json(products)
+    })
+
+}
+
 module.exports={
 
-    create_product
+    create_product,
+    get_all_products
 }
