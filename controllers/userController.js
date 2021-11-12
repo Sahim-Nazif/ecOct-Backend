84b8708b7a17f7257ba1911f35eee0f7ac3c17a8
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-11-11 20:45:30 -0500

    sign up controller and api call updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,18 +2,22 @@ const User=require('../models/userModel')
 
 const register_user=async(req, res)=>{
 
-    const {email}=req.body
+    const {name, email, password}=req.body
     const emailExits=await User.findOne({email})
     
     if (emailExits) {
-        res.send('Looks like this user already exists!')
+        res.status(400).send('Looks like this user already exists!')
     } 
     else {
-        const user= new User(req.body)
+        const user= new User({
+            name,
+            email,
+            password
+        })
         user.save(error=>{
 
             if (!error ) {
-                res.send('Hey Congrats! You all set')
+                 res.send('Hey Congrats! You all set')
             } else {
     
                 res.send('Oh no ! Somethign went wrong,please try again')
