842fa6f10df27b17fe4dc63daa5ec117f3332a1d
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-12-02 20:54:58 -0500

    created stripe payment gateway

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,35 +1,37 @@
-const stripe=require('stripe')(process.env.STRIPE_SECRET)
+const stripe = require('stripe')(process.env.STRIPE_SECRET)
 const uuidv4 = require('uuid').v4
 
 
-const charges_stripe=async(req, res)=>{
+const charges_stripe = (req, res) => {
 
-    const {token, cartItems, currentUser,cartTotal }=req.body;
 
-    const customer=await stripe.customers.create({
-        email:token.email,
-        source:token.id
-    })
+    const { token, cartTotal } = req.body;
+    try {
+        stripe.customers
+            .create({
+                name: token.name,
+                email: token.email,
+                source: token.id
+            })
+            .then(customer =>
 
-    const payment=await stripe.charges.create({
+                stripe.charges.create({
 
-        amount:cartTotal,
-        currency:'CA',
-        customer:customer,
-        receipt_email:token.email
-    }, {
-        itempotencyKey:uuidv4()
-    })
+                    amount: cartTotal * 100,
+                    currency: 'cad',
+                    customer: customer.id,
+                    receipt_email: token.email
 
-    if(payment) {
-        res.send('Payment successful')
-    } else {
+                }, {
+                    idempotencyKey: uuidv4()
+                })
+            )
+            .then(() => res.send('Payment successful'))
+            .catch(err => console.log(err));
+    } catch (err) {
         return res.status(400).json({message:'Payment failed'})
     }
-
 }
-
-
 
 
 module.exports = {
