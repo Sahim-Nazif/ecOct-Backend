c4b787a615a31a7dbf2e85072b10b71debdf5ed2
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-12-19 14:51:24 -0500

    added getOrderById to router

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const {charges_stripe, getOrdersByUserId}=require('../controllers/orderController')
+const {charges_stripe, getOrdersByUserId, getOrderById}=require('../controllers/orderController')
 
 
 router.post('/orders/placeOrder', charges_stripe)
 router.get('/orders/getordersbyUserid', getOrdersByUserId)
+router.get('orders/getorderbyid', getOrderById)
 
 module.exports=router;
